10be775a790c9abf65760efe4f27d9b249743c86
andreysava19 <andreysava26@gmail.com>
2024-03-22 17:52:34 -0600

    Check a few errors about read the futures files

--- t3a_experiments/Experiment_1/Broad_Output_Dataset_Creator_f.py
+++ t3a_experiments/Experiment_1/Broad_Output_Dataset_Creator_f.py
@@ -49,7 +49,98 @@ if run_for_first_time == True:
 
 ############################################################################################################
 df_0_output = pd.read_csv(params['Executables_2'] + params['Out_dat_0'], index_col=None, header=0)
-df_f_output = pd.read_csv( file_adress + params['Futures_3'] + params['Out_dat_f'], index_col=None, header=0)
+
+
+# In case if you use solver='glpk' and glpk='old' uncomment this section
+#----------------------------------------------------------------------------------------------------------#
+# Use this space to edit the column names of future zero resuls:
+# df_0_output = df_0_output.rename(columns={'Run.ID': 'Strategy'})
+
+# df_0_output['Strategy'] = df_0_output['Strategy'].replace('DDP50', 'DDP')
+
+# df_0_output.insert(1, 'Future.ID', [0]*len(df_0_output.index.tolist()))
+# #df_0_output['Capex2023'] = df_0_output['DiscountedCapitalInvestment']
+# #df_0_output['FixedOpex2023'] = #df_0_output['DiscountedOperatingCost']
+# #df_0_output['VarOpex2023'] = #df_0_output['DiscountedOperatingCost']
+# #df_0_output['Opex2023'] = df_0_output['DiscountedOperatingCost']
+# #df_0_output['Externalities2023'] = df_0_output['DiscountedTechnologyEmissionsPenalty']
+# #df_0_output['Capex_GDP'] = [0]*len(df_0_output.index.tolist())
+# #df_0_output['FixedOpex_GDP'] = [0]*len(df_0_output.index.tolist())
+# #df_0_output['VarOpex_GDP'] = [0]*len(df_0_output.index.tolist())
+# #df_0_output['Opex_GDP'] = [0]*len(df_0_output.index.tolist())
+# #df_0_output['Externalities_GDP'] = [0]*len(df_0_output.index.tolist())
+# df_0_output['Scen_fut'] = df_0_output['Strategy'].astype(str) + "_" + df_0_output['Future.ID'].astype(str)
+
+# valid_column_names = [
+#     "Strategy",
+#     "Future.ID",
+#     "Fuel",
+#     "Technology",
+#     "Emission",
+#     "Year",
+#     "Demand",
+#     "NewCapacity",
+#     "AccumulatedNewCapacity",
+#     "TotalCapacityAnnual",
+#     "TotalTechnologyAnnualActivity",
+#     "ProductionByTechnology",
+#     "UseByTechnology",
+#     "CapitalInvestment",
+#     "DiscountedCapitalInvestment",
+#     "SalvageValue", #
+#     "DiscountedSalvageValue", #
+#     "OperatingCost",
+#     "DiscountedOperatingCost",
+#     "AnnualVariableOperatingCost",
+#     "AnnualFixedOperatingCost", #
+#     "TotalDiscountedCostByTechnology",
+#     "TotalDiscountedCost",
+#     "AnnualTechnologyEmission",
+#     'AnnualTechnologyEmissionPenaltyByEmission',
+#     'AnnualTechnologyEmissionsPenalty',
+#     'DiscountedTechnologyEmissionsPenalty',
+#     "AnnualEmissions",
+#     "Capex2021",
+#     'FixedOpex2021', #
+#     "VarOpex2021",
+#     "Opex2021",
+#     'Externalities2021',
+#     "Capex_GDP",
+#     "FixedOpex_GDP", #
+#     "VarOpex_GDP",
+#     "Opex_GDP",
+#     'Externalities_GDP',
+#     'Scen_fut']
+
+# df_0_output = df_0_output[valid_column_names]
+#----------------------------------------------------------------------------------------------------------#
+
+
+df_f_output = pd.read_csv( '.' + params['Futures_3'] + params['Out_dat_f'], index_col=None, header=0, low_memory=False)
+
+
+# In case if you use solver='glpk' and glpk='old' uncomment this section
+#----------------------------------------------------------------------------------------------------------#
+# null_columns_f, valid_columns_f = [], []
+# for acol in df_f_output.columns.tolist():
+#     # Check if all values in the column are null
+#     if df_f_output[acol].isnull().all():
+#         null_columns_f.append(acol)
+#     else:
+#         valid_columns_f.append(acol)
+
+# #df_f_output = df_f_output[valid_columns_f]
+# df_f_output = df_f_output[valid_column_names]
+
+# if df_0_output.columns.tolist() != df_f_output.columns.tolist():
+#     print('Error')
+#     sys.exit()
+# else:
+#     print('Success')
+
+# df_0_output["Year"]=df_0_output["Year"].fillna(0).astype(int)
+#----------------------------------------------------------------------------------------------------------#
+
 li_output = [df_0_output, df_f_output]
 #
 df_output = pd.concat(li_output, axis=0, ignore_index=True)
@@ -57,7 +148,12 @@ df_output.sort_values(by=params['by_1'], inplace=True)
 ############################################################################################################
 df_0_input = pd.read_csv(params['Executables_2'] + params['In_dat_0'], index_col=None, header=0)
 
-df_f_input = pd.read_csv('.' + params['Futures_3'] + params['In_dat_f'], index_col=None, header=0)
+# In case if you use solver='glpk' and glpk='old' uncomment this section
+#----------------------------------------------------------------------------------------------------------#
+# df_0_input['Strategy'] = df_0_input['Strategy'].replace('DDP50', 'DDP')
+#----------------------------------------------------------------------------------------------------------#
+
+df_f_input = pd.read_csv('.' + params['Futures_3'] + params['In_dat_f'], index_col=None, header=0, low_memory=False)
 li_intput = [df_0_input, df_f_input]
 #
 df_input = pd.concat(li_intput, axis=0, ignore_index=True)
