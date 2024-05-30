a81fcc67ae92b15d1d97c2bde5c730907f950499
andreysava19 <andreysava26@gmail.com>
2024-05-30 15:30:11 -0600

    Update concatenate script, en delete some columns of the output files

--- config_main_files/change_default_vals_params.py
+++ config_main_files/change_default_vals_params.py
@@ -29,65 +29,84 @@ def get_config_main_path(full_path, base_folder='config_main_files'):
     
     return appended_path
 
-# Read yaml file with parameterization
-file_config_address = get_config_main_path(os.path.abspath(''))
-# sys.exit()
-with open(file_config_address + '\\' + 'MOMF_B1_exp_manager.yaml', 'r') as file:
-    # Load content file
-    params = yaml.safe_load(file)
-# Select dict with default values parameters
-default_val_params = params['default_val_params']
-default_val_sets = params['sets_otoole']
 
-# Create defaul yaml file by otoole
-file_path = file_config_address + 'config\\conversion_format.yaml'
-directory = os.path.dirname(file_path)
-if not os.path.exists(directory):
-    os.makedirs(directory)
+if __name__ == '__main__':
+
+    # Parameters otoole has by default but don't use them in MOMF
+    parameters_otoole_no_momf = [
+        "AnnualTechnologyEmissionByMode",
+        "ProductionByTechnologyAnnual",
+        "RateOfActivity",
+        "RateOfProductionByTechnology",
+        "RateOfUseByTechnology",
+        "RateOfUseByTechnologyByMode",
+        "TotalAnnualTechnologyActivityByMode",
+        "TotalTechnologyModelPeriodActivity"
+    ]
     
-os.system(f'otoole setup --overwrite config {file_path}')
-
-# Create folder with csv templates
-os.system(f'otoole setup --overwrite csv ' + file_config_address + 'config\\templates')
-
-# Read yaml file with parameterization to conversion format by otoole
-with open(file_path, 'r') as file:
-    # Load content file
-    default_format = yaml.safe_load(file)
-
-# Create temporal variable to delete some parameter from 'default_format'
-temp_defaut_params = deepcopy(default_format)
-
-# Delete parameters that are not in 'MOMF_T1_B1.yaml'
-for old_key, attributes in temp_defaut_params.items():
-    if attributes['type'] == 'param':
-        for index in attributes['indices']:
-            if index not in default_val_sets:
-                default_format[old_key]['indices'].remove(index)
-                    
-        if not old_key in default_val_params:
-            default_format.pop(old_key)
+    # Read yaml file with parameterization
+    file_config_address = get_config_main_path(os.path.abspath(''))
+    # sys.exit()
+    with open(file_config_address + '\\' + 'MOMF_B1_exp_manager.yaml', 'r') as file:
+        # Load content file
+        params = yaml.safe_load(file)
+    # Select dict with default values parameters
+    default_val_params = params['default_val_params']
+    default_val_sets = params['sets_otoole']
+    
+    # Create defaul yaml file by otoole
+    file_path = file_config_address + 'config\\conversion_format.yaml'
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        
+    os.system(f'otoole setup --overwrite config {file_path}')
+    
+    # Create folder with csv templates
+    os.system(f'otoole setup --overwrite csv {file_config_address}config\\templates')
+    
+    # Read yaml file with parameterization to conversion format by otoole
+    with open(file_path, 'r') as file:
+        # Load content file
+        default_format = yaml.safe_load(file)
+    
+    # Create temporal variable to delete some parameter from 'default_format'
+    temp_default_params = deepcopy(default_format)
+    
+    # Delete parameters that are not in 'MOMF_T1_B1.yaml'
+    for old_key, attributes in temp_default_params.items():
+        if attributes['type'] == 'param':
+            for index in attributes['indices']:
+                if index not in default_val_sets:
+                    default_format[old_key]['indices'].remove(index)
+                        
+            if not old_key in default_val_params:
+                default_format.pop(old_key)
+                shutil.os.remove(file_config_address + 'config\\templates\\' + old_key + '.csv')
+            
+        elif attributes['type'] == 'set' and not old_key in default_val_sets:
+            a = default_format.pop(old_key)
             shutil.os.remove(file_config_address + 'config\\templates\\' + old_key + '.csv')
         
-    elif attributes['type'] == 'set' and not old_key in default_val_sets:
-        a = default_format.pop(old_key)
-        shutil.os.remove(file_config_address + 'config\\templates\\' + old_key + '.csv')
+        elif attributes['type'] == 'result':
+            for index in attributes['indices']:
+                if index not in default_val_sets:
+                    default_format[old_key]['indices'].remove(index)
+                    # shutil.os.remove(file_config_address + 'config\\templates\\' + index + '.csv')      
     
-    elif attributes['type'] == 'result':
-        for index in attributes['indices']:
-            if index not in default_val_sets:
-                default_format[old_key]['indices'].remove(index)
-                # shutil.os.remove(file_config_address + 'config\\templates\\' + index + '.csv')
+    # Delete key asociate with the variable "parameters_otoole_no_momf"
+    cleaned_default_format = {k: v for k, v in default_format.items() if k not in parameters_otoole_no_momf}
+    
+    # Assing dict clean of the parameters aren't useful to default_format dict
+    default_format = cleaned_default_format
         
-    
+                
+    # Change default value
+    for old_key, attributes in default_format.items():
+        if attributes['type'] == 'param' and 'default' in attributes:
+            default_val_params = params['default_val_params']
+            default_format[old_key]['default'] = default_val_params[old_key]
             
-        
-# Change default value
-for old_key, attributes in default_format.items():
-    if attributes['type'] == 'param' and 'default' in attributes:
-        default_val_params = params['default_val_params']
-        default_format[old_key]['default'] = default_val_params[old_key]
-        
-# Save changes of default values in the yaml file
-with open(file_path, 'w') as file:
-    yaml.dump(default_format, file, sort_keys=False)
+    # Save changes of default values in the yaml file
+    with open(file_path, 'w') as file:
+        yaml.dump(default_format, file, sort_keys=False)
