2b40f09f40183927ed7a5635948570aa4be75a4a
andreysava19 <andreysava26@gmail.com>
2024-07-11 09:12:01 -0600

    Update default values of the parameters into the yaml file. B1 and EM have diferents default values, specific the large value, EM has larger max value than B1. If you use the same for both you have infeasibilities

---
+++ tests/Waste/t3a_experiments/Experiment_1/Auxiliares.py
@@ -0,0 +1,131 @@
+def interpolation_non_linear_final_lock(time_list, value_list, new_relative_final_value, finyear, Initial_Year_of_Uncertainty):
+    # Rememeber that the 'old_relative_final_value' is 1
+    old_relative_final_value = 1
+    new_value_list = []
+    # We select a list that goes from the "Initial_Year_of_Uncertainty" to the Final Year of the Time Series
+    initial_year_index = time_list.index( Initial_Year_of_Uncertainty )
+    fraction_time_list = time_list[initial_year_index:]
+    fraction_value_list = value_list[initial_year_index:]
+
+    # Subtract the time between the last year and the "finyear":
+    diff_yrs = time_list[-1] - finyear
+
+    # We now perform the 'non-linear OR linear adjustment':
+    xdata = [ fraction_time_list[i] - fraction_time_list[0] for i in range(len(fraction_time_list) - diff_yrs)]
+    ydata = [ float( fraction_value_list[i] ) for i in range(len(fraction_value_list) - diff_yrs)]
+    ydata_whole = [ float( fraction_value_list[i] ) for i in range(len(fraction_value_list))]
+    delta_ydata = [ ydata_whole[i]-ydata_whole[i-1] for i in range( 1,len( ydata_whole ) ) ]
+    #
+    m_original = ( ydata[-1]-ydata[0] ) / ( xdata[-1]-xdata[0] )
+    #
+    m_new = ( ydata[-1]*(new_relative_final_value/old_relative_final_value) - ydata[0] ) / ( xdata[-1]-xdata[0] )
+    #
+    if int(m_original) == 0:
+        delta_ydata_new = [m_new for i in range( 0,len( ydata_whole ) ) ]
+    else:
+        delta_ydata_new = [ (m_new/m_original)*(ydata[i]-ydata[i-1]) for i in range( 0,len( ydata ) ) ]
+    #
+    ydata_new = [0 for i in range(len(ydata_whole))]
+    # ydata_new[0] = ydata_whole[0]
+    list_apply_delta_ydata_new = []
+    
+    for i in range( 0, len( delta_ydata )+1 ):
+        if time_list[i+initial_year_index] <= finyear:
+            apply_delta_ydata_new = delta_ydata_new[i]
+        else:
+            apply_delta_ydata_new = 0
+        list_apply_delta_ydata_new.append(apply_delta_ydata_new)
+        
+        if i == 0:
+            ydata_new[i] = ydata_whole[0] + apply_delta_ydata_new
+        else:
+            ydata_new[i] = ydata_new[i-1] + apply_delta_ydata_new
+    #
+    # We now recreate the new_value_list considering the fraction before and after the Initial_Year_of_Uncertainty
+    fraction_list_counter = 0
+    for n in range( len( time_list ) ):
+        if time_list[n] >= Initial_Year_of_Uncertainty:
+            new_value_list.append( ydata_new[ fraction_list_counter ] )
+            # print(time_list[n], ydata_new[ fraction_list_counter ], value_list[n], fraction_list_counter)
+            fraction_list_counter += 1
+        else:
+            new_value_list.append( float( value_list[n] ) )
+
+    return new_value_list, list_apply_delta_ydata_new, fraction_list_counter
+
+def interpolation_non_linear_final(time_list, value_list, new_relative_final_value, finyear, Initial_Year_of_Uncertainty):
+    # Rememeber that the 'old_relative_final_value' is 1
+    old_relative_final_value = 1
+    new_value_list = []
+    # We select a list that goes from the "Initial_Year_of_Uncertainty" to the Final Year of the Time Series
+    initial_year_index = time_list.index( Initial_Year_of_Uncertainty )
+    fraction_time_list = time_list[initial_year_index:]
+    fraction_value_list = value_list[initial_year_index:]
+
+    # Subtract the time between the last year and the "finyear":
+    diff_yrs = time_list[-1] - finyear
+
+    # We now perform the 'non-linear OR linear adjustment':
+    xdata = [ fraction_time_list[i] - fraction_time_list[0] for i in range(len(fraction_time_list) - diff_yrs)]
+    ydata = [ float( fraction_value_list[i] ) for i in range(len(fraction_value_list) - diff_yrs)]
+    ydata_whole = [ float( fraction_value_list[i] ) for i in range(len(fraction_value_list))]
+    delta_ydata = [ ydata_whole[i]-ydata_whole[i-1] for i in range( 1,len( ydata_whole ) ) ]
+    #
+    m_original = ( ydata[-1]-ydata[0] ) / ( xdata[-1]-xdata[0] )
+    #
+    m_new = ( ydata[-1]*(new_relative_final_value/old_relative_final_value) - ydata[0] ) / ( xdata[-1]-xdata[0] )
+    #
+    if int(m_original) == 0:
+        delta_ydata_new = [m_new for i in range( 0,len( ydata_whole ) ) ]
+    else:
+        delta_ydata_new = [ (m_new/m_original)*(ydata_whole[i]-ydata_whole[i-1]) for i in range( 1 ,len( ydata_whole ) ) ]
+        delta_ydata_new = [0] + delta_ydata_new
+    #
+    ydata_new = [0 for i in range(len(ydata_whole))]
+    # ydata_new[0] = ydata_whole[0]
+    list_apply_delta_ydata_new = []
+
+    for i in range( 0, len( delta_ydata )+1 ):
+        if time_list[i+initial_year_index] <= finyear:
+            apply_delta_ydata_new = delta_ydata_new[i]
+            # print(i, m_original)
+        else:
+            apply_delta_ydata_new = sum(delta_ydata_new)/len(delta_ydata_new)
+        list_apply_delta_ydata_new.append(apply_delta_ydata_new)
+        
+        if i == 0:
+            ydata_new[i] = ydata_whole[0] + apply_delta_ydata_new          
+        else:
+            ydata_new[i] = ydata_new[i-1] + apply_delta_ydata_new
+    #
+    # We now recreate the new_value_list considering the fraction before and after the Initial_Year_of_Uncertainty
+    fraction_list_counter = 0
+    for n in range( len( time_list ) ):
+        if time_list[n] >= Initial_Year_of_Uncertainty:
+            new_value_list.append( ydata_new[ fraction_list_counter ] )
+            # print(time_list[n], ydata_new[ fraction_list_counter ], value_list[n], fraction_list_counter)
+            fraction_list_counter += 1
+        else:
+            new_value_list.append( float( value_list[n] ) )
+            # print(time_list[n], float( value_list[n] ))
+    #
+    # print('\n\n')
+    # We return the list:
+    return new_value_list
+
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+
+def time_series_shift( time_list, value_list, new_relative_initial_value ):
+    new_value_list = []
+    # We multiply the initial value of value_list times the new_relative_value
+    new_initial_value = value_list[0]*new_relative_initial_value
+    shift_value = new_initial_value - value_list[0]
+    #
+    for n in range(len( time_list ) ):
+        new_value_list.append( value_list[n] + shift_value )
+    #
+    # We return the list:
+    return new_value_list
+
