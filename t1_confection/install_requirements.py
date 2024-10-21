81564728aebbc6bc714e1f274985dd7537c370f6
andreysava19 <andreysava26@gmail.com>
2024-10-21 11:25:43 -0600

    Include new module to install MOMF dependencies

---
+++ t1_confection/install_requirements.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 12:36:41 2024
+
+@author: Climate Lead Group; Andrey Salazar-Vargas,
+        Luis Victor-Gallardo, Jairo Quirós-Tortós
+"""
+
+from setup_utils import install_requirements 
+
+if __name__ == '__main__':
+    requirements_file = 'requirements.txt'
+    install_requirements(requirements_file)
